bc7d185d5e3d9888ccbc223e1b87beea188d40c9
Rohith Uppala <rohith.uppala@booking.com>
2018-09-16 11:09:03 +0200

    Q - Learning and modifications to SARSA

--- temporal_differencing/q_learning.py
+++ temporal_differencing/q_learning.py
@@ -1,5 +1,10 @@
 '''
-SARSA (State - Action - Reward - State - Action)
+Q - learning ( Known as Temporal differencing with control using an off-policy mechanism)
+
+Here, we learn through observation. No updates of policy matrix while trying to acheive
+the convergence
+
+Q(st, at) <- Q(st, at) + alpha * ( r(t+1)  + gamma * (max(q(st+1, at+1))) - q(st, at) )
 
 @Author: Rohith Uppala
 '''
@@ -7,13 +12,20 @@ SARSA (State - Action - Reward - State - Action)
 import numpy as np
 from gridworld import GridWorld
 
-def update_state_action_matrix(state_action_matrix, reward, gamma, observation, new_observation, action, new_action, visit_counter_matrix):
+def update_state_action_matrix(state_action_matrix, reward, gamma, observation, new_observation, action, visit_counter_matrix):
 	col = observation[1] + (4 * observation[0])
 	col_t1 = new_observation[1] + (4 * new_observation[0])
+
+        q = state_action_matrix[action, col]
+        # This is the only difference between SARSA and Q - Learning instead of incremental learning
+        # We usually go with the sub-optimal policy
+        q_t1 = np.max(state_action_matrix[:, col_t1])
+
         # Instead of going for a constant, let's calculate based on how many times we have visited this state
 	alpha = 1 / (1.0 + visit_counter_matrix[action, col])
-        state_action_matrix[action, col] = state_action_matrix[action, col] + alpha * ( reward + gamma * state_action_matrix[new_action, col_t1] - state_action_matrix[action, col])
+        state_action_matrix[action, col] = state_action_matrix[action, col] + alpha * ( reward + gamma * q_t1 - q)
         return state_action_matrix
+
 
 def update_policy(policy_matrix, state_action_matrix, observation):
 	col = observation[1] + (observation[0] * 4)
@@ -28,8 +40,8 @@ def update_visit_counter(visit_counter_matrix, observation, action):
 
 def return_epsilon_greedy_action(policy_matrix, observation, epsilon=0.1):
 	tot_actions = int(np.nanmax(policy_matrix) + 1)
-	action = int(policy_matrix[observation[0]][observation[1]])
-	non_greedy_prob = epsilon / tot_actions
+        action = int(policy_matrix[observation[0]][observation[1]])
+        non_greedy_prob = epsilon / tot_actions
 	greedy_prob = 1 - epsilon  + non_greedy_prob
 	weight_array = np.full((tot_actions), non_greedy_prob)
 	weight_array[action] = greedy_prob
@@ -66,7 +78,17 @@ def main():
 	reward_matrix[1, 3] = -1
 
 	transition_matrix = np.array([[0.8, 0.1, 0.0, 0.1], [0.1, 0.8, 0.1, 0.0], [0.0, 0.1, 0.8, 0.1], [0.1, 0.0, 0.1, 0.8]])
-	policy_matrix = np.array([[1, 1, 1, -1], [0, np.NaN, 0, -1], [0, 3, 3, 3]])
+
+        # Generating random policy
+        policy_matrix = np.random.randint(low=0, high=4, size=(3, 4)).astype(np.float32)
+        policy_matrix[1,1] = np.NaN
+        policy_matrix[0, 3] = policy_matrix[1, 3] = -1
+        print "Random policy matrix"
+        print_policy(policy_matrix)
+
+        exploratory_policy_matrix = np.array([[1, 1, 1, -1], [0, np.NaN, 0, -1], [0, 1, 0, 3]])
+        print "Exploratory policy matrix"
+        print_policy(exploratory_policy_matrix)
 
 	env.setStateMatrix(state_matrix)
 	env.setRewardMatrix(reward_matrix)
@@ -84,15 +106,14 @@ def main():
 		is_starting = True
 
 		for step in range(1000):
-			action = return_epsilon_greedy_action(policy_matrix, observation, epsilon=0.1)
+			action = return_epsilon_greedy_action(exploratory_policy_matrix, observation, epsilon=0.1)
 			if is_starting:
 				action = np.random.randint(0, 4)
 				is_starting = False
 
 			new_observation, reward, done = env.step(action)
-			new_action = int(policy_matrix[new_observation[0]][new_observation[1]])
 
-			state_action_matrix = update_state_action_matrix(state_action_matrix, reward, gamma, observation, new_observation, action, new_action, visit_counter_matrix)
+			state_action_matrix = update_state_action_matrix(state_action_matrix, reward, gamma, observation, new_observation, action, visit_counter_matrix)
 			policy_matrix = update_policy(policy_matrix, state_action_matrix, observation)
 			visit_counter_matrix = update_visit_counter(visit_counter_matrix, observation, action)
 
